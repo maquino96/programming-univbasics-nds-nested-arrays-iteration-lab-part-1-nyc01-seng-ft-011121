2cf8c5a6513e5a0f436b39d27ec9086849b5a1d9
Matthew Aquino <matthewaquino96@gmail.com>
2020-12-31 19:54:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,9 @@ def find_even_values(src)
     while element_index < src[row_index].count do
       if src[row_index][element_index]%2 == 0 
         puts src[row_index][element_index]
+        
         #result_array.push(src[row_index][element_index])
+        
       end
     end
   end
